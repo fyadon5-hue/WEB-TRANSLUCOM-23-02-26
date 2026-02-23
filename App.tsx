52156721b2a88f79b4134399ae8de23814777fef
fyadon5-hue <fyadon5@gmail.com>
2026-02-23 04:36:37 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,23 @@
+import Hero from "./Hero";
+import About from "./About";
+import Logos from "./Logos";
+import News from "./News";
+import QuoteForm from "./QuoteForm";
+import AiTools from "./AiTools";
+import Footer from "./Footer";
+import WhatsAppButton from "./WhatsAppButton";
+
 export default function App() {
   return (
-    <div style={{ padding: "40px", color: "white", background: "#0f172a" }}>
-      <h1>Translucom funcionando 🚚</h1>
-      <p>Deploy exitoso en Netlify</p>
-    </div>
+    <>
+      <Hero />
+      <About />
+      <Logos />
+      <News />
+      <QuoteForm />
+      <AiTools />
+      <Footer />
+      <WhatsAppButton />
+    </>
   );
 }
