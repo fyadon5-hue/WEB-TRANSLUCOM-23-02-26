1bd17a953689347e3d3b40ac09290e9a1531970a
fyadon5-hue <fyadon5@gmail.com>
2026-02-23 04:32:15 -0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", color: "white", background: "#0f172a" }}>
+      <h1>Translucom funcionando 🚚</h1>
+      <p>Deploy exitoso en Netlify</p>
+    </div>
+  );
+}
